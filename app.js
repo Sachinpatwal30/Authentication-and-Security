0e19b6e7e8efb7c97b1b95de9701d3b8cf85e68e
sachinpatwal30 <sachinpatwal30@gmail.com>
2022-10-20 17:12:24 +0530

    Level 2 Encryption

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser= require("body-parser");
 const ejs= require("ejs");
 const mongoose= require("mongoose");
+const encrypt = require('mongoose-encryption');
+
+
 
 
 const app= express();
@@ -12,11 +16,15 @@ app.use(bodyParser.urlencoded({extended:true}))
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 
-const userSchema={
+const userSchema= new mongoose.Schema({
 
     email:String,
     password:String
-};
+});
+
+
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields: ["password"] });
 
 const User= mongoose.model("User",userSchema);
 
